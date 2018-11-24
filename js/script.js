79385fa0c31c8f2680c5834aa1582b64e3d979f3
M S home <mariaserg@gmail.com>
2018-11-24 18:36:34 +0200

    Lesson 2 Quiz 5 done

--- js/script.js
+++ js/script.js
@@ -11,7 +11,8 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    var address = $('#street').val() + ', ' + $('#city').val();
+    var city = $('#city').val();
+    var address = $('#street').val() + ', ' + city;
 
     $greeting.text('So, you want to live at ' + address + '?');
 
@@ -20,7 +21,30 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + bgImgSrc + '">');
     
     // load nytimes
-    
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    url += "?" + $.param({
+        "api-key": "171cbf5c173a4064acec4f8731e0febf",
+        "sort": "newest",
+        "q": city
+    });
+    $.getJSON(url, function (data) {
+        //console.log(data);
+        $nytHeaderElem.text("New York Times articles about" + city);
+
+        var articles = data.response.docs;
+        var items = [];
+        $.each( articles, function( num, article ) {            
+            var link="<a href='" + article.web_url + "'>" + article.headline.main + "</a>";
+            var desc = "<p>" + article.snippet + "</p>";
+            items.push( "<li class='article'>" + link + desc + "</li>" );
+        });
+        
+        $nytElem.append(items.join( "" ));
+       
+    })
+    .fail(function() {
+        $nytHeaderElem.text("New York Times articles could not be loaded");
+      });;
 
     return false;
 };
